dcc0a46be50cf08adf4abb949e0960349d381de1
magedzidan <magedzidan@yahoo.com>
2023-03-16 16:45:24 +0200

    Update constance.dart

--- lib/constant/constance.dart
+++ lib/constant/constance.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
-const primarycolor=Color.fromRGBO(24,76,114,255);
-const secondrycolor=Color.fromRGBO(52,115,130,255);
+const primarycolor=Color(0xFF174771);
+const secondrycolor=Color(0xFF337183);
+
 
 
