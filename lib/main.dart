02fffe01bdd5ebcc5d6d8426ea1597dc3d7005c7
magedzidan <magedzidan@yahoo.com>
2023-03-17 15:24:20 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:get/get.dart';
 import 'package:youth_care_application/authentcation/sign_in/sign_in_viewpage.dart';
-import 'options/firebase_options.dart';
+import 'package:youth_care_application/constant/constance.dart';
+import 'package:youth_care_application/helper/binding.dart';
+import 'firebase_options.dart';
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
@@ -17,8 +19,13 @@ class MyApp extends StatelessWidget{
   Widget build(BuildContext context){
     return GetMaterialApp(
 
-      //initialBinding: Binding(),
+      initialBinding: Binding(),
       debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primaryColor: primarycolor,
+        //scaffoldBackgroundColor: primarycolor,
+
+      ),
       home:SignIn(),
       /*routes:{
           "addnotes":(context) => addnotes()
